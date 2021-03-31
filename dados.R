f16a0c68025fd5309bd5d37c058e501be8503c00
rogcolorado <32040649+rogcolorado@users.noreply.github.com>
2021-03-31 11:32:08 -0300

    Atualizado 31/03/2021

--- dados.R
+++ dados.R
@@ -31,3 +31,49 @@ carga_h = as.factor(c(220, 220, 150, 100, 100))#separa os valores iguais em cate
 summary(carga_h)
 mode(carga_h)
 class(carga_h)
+
+# ARMAZENAMENTO LOGICO
+l1 = salario > horas
+l1
+l2 = salario < horas
+l2
+logico = TRUE
+logico
+logico1 = "TRUE"
+logico1
+logico2 = c(1, TRUE, 3)
+logico2
+
+# VETORES
+# vetores de characterees
+is.vector(nomes)
+mode(nomes)
+
+# vetores umericos
+is.vector(horas)
+mode(horas)
+
+# vetores logicos
+is.vector(l1)
+mode(l1)
+
+# LISTAS = vetor com tipos de dados diferentes
+a = c(1,2,3,4,5)
+b = c(1,"2",3,"4",5)
+a
+b
+
+is.list(a)
+is.list(b)
+
+is.vector(a)
+is.vector(b)
+
+li = list(10,"2",6)
+is.list(li)
+mode(li)
+str(li)
+
+e = list(c(10,6,51,5),"2",8)
+str(e)
+e[[1]][1] # busca valores na lista, no caso primeiro do vetor
